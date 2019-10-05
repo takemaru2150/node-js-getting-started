07e96da0c7d58f6a5e81c0ef41b5f59e53ea37ad
sakurai <takeshi2jp1000@gmail.com>
2019-10-05 11:59:58 +0900

    add api

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// ーーー初期設定ーーー
+
 // const express = require('express')
 // const path = require('path')
 // const PORT = process.env.PORT || 5000
@@ -9,7 +11,24 @@
 //   .get('/', (req, res) => res.render('pages/index'))
 //   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-const cool = require('cool-ascii-faces')
+
+// ーーーランダムで顔文字を表示させるーーー
+// const cool = require('cool-ascii-faces')
+// const express = require('express')
+// const path = require('path')
+// const PORT = process.env.PORT || 5000
+
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .get('/cool', (req, res) => res.send(cool()))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+  // ーーーRest API開発ーーー
+
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -19,5 +38,6 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
+  .get('/g/', (req, res) => res.json({method: "こんにちは、getさん"})) // 追加
+  .post('/p/', (req, res) => res.json({method: "こんにちは、postさん"})) // 追加
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
